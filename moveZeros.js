221c6ac6fb9e17e0335fff100e11e05a3aac7b23
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-08 22:51:36 +0200

    added solution to moveZeros.js

--- moveZeros.js
+++ moveZeros.js
@@ -14,5 +14,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    
+  for(let i = nums.length - 1; i >= 0; i--) {
+    if(nums[i] === 0 ) {
+        nums.splice(i, 1);
+        nums.push(0);
+    }
+  }
+  return nums;
 };
