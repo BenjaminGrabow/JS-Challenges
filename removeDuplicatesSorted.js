e4b79e67afe4fa085172c00b66a6c69ff57cba7c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-08 21:44:16 +0200

    added solution to removeDuplicatesSorted.js

--- removeDuplicatesSorted.js
+++ removeDuplicatesSorted.js
@@ -32,3 +32,17 @@
 // for (int i = 0; i < len; i++) {
 //     print(nums[i]);
 // }
+
+
+var removeDuplicates = function(nums) {
+  let head = 0;
+   
+   for(let i = 1; i < nums.length; i++) {
+      if(nums[i] !== nums[head]) {
+          head++;
+          nums[head] = nums[i];
+      }  
+  } 
+   
+   return head + 1;
+};
