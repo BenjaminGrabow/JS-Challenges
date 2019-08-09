38e166cbbd048d9e57b5c506f7f93ab84fae5439
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-09 20:07:34 +0200

    added solution to capitalieLetters.js

---
+++ capitalizeLetters.js
@@ -0,0 +1,15 @@
+const capitalLetters = (s) => {
+  
+// of course also with for loop possible ! 
+
+  return s
+    .split(' ')
+    .map(char =>
+      char
+      .substring(0, 1)
+      .toUpperCase() + char
+      .substring(1))
+      .join(' ');
+};
+
+capitalLetters('hello bye yeah');
