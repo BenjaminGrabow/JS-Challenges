c98989b2a6655534229c7e7e848c866936f849d9
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-09 18:54:54 +0200

    added 2 solutions to reverseString.js with new es6 syntax

--- reverseString.js
+++ reverseString.js
@@ -7,15 +7,32 @@ const reverseString = (str) => {
 // let revString = '';
 
 // for(let i = str.length - 1; i >= 0; i--){
-//   revString += str[i];
+//   revString += str[i];  ///// or: revString = revString + str[i];
 // }
 
 // return revString;
 
 ///////////////////////////////////////////////////////////
 
+// let revString = '';
+
+// for(let i = 0; i < str.length; i++){
+//   revString = str[i] + revString;
+// }
+
+// return revString;
 
 
+/////////////////////////////////////////////////////////////
+
+// ES6 Syntax => more impressive !
+let revString = '';
+
+for(let char of str){
+  revString = char + revString;
+}
+
+return revString;
 
 
 };
