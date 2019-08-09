5a7c79e9f21bce5b7658d28a3b8ee01285a037ed
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-09 20:43:18 +0200

    added solution to maxCharacter.js

---
+++ maxCharacter.js
@@ -0,0 +1,19 @@
+const maxCharacter = (s) => {
+   let charMap = {};
+   let maxNumber = 0;
+   let maxChar = '';
+
+   s.split('').forEach(char => {
+  if(charMap[char]) charMap[char]++;
+  else charMap[char] = 1;
+});
+
+for(let char in charMap) {
+ if(charMap[char] > maxNumber) maxNumber = charMap[char];
+ maxChar = char;
+}
+
+return maxChar;
+}
+
+maxCharacter('javascripttt');
