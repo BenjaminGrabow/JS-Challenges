89408151396d36f200922b992a05ec24b8c503b4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-09 20:12:12 +0200

    added solution to capitalieLetters.js with regEx

--- capitalizeLetters.js
+++ capitalizeLetters.js
@@ -2,14 +2,20 @@ const capitalLetters = (s) => {
   
 // of course also with for loop possible ! 
 
-  return s
-    .split(' ')
-    .map(char =>
-      char
-      .substring(0, 1) /// or char[0]
-      .toUpperCase() + char
-      .substring(1))
-      .join(' ');
+  // return s
+  //   .split(' ')
+  //   .map(char =>
+  //     char
+  //     .substring(0, 1) /// or char[0]
+  //     .toUpperCase() + char
+  //     .substring(1))
+  //     .join(' ');
+
+  ///////////////////////////////////////////////////
+
+  // regular expression
+
+  return s.replace(/\b[a-z]/gi, (char) => char.toUpperCase());
 };
 
 capitalLetters('hello bye yeah');
