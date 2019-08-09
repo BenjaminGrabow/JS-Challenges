3a867c71bf2b2d3b2d34468239d17832e2ab687f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-09 18:46:05 +0200

    added 2 solutions to reverseString.js

---
+++ reverseString.js
@@ -0,0 +1,25 @@
+const reverseString = (str) => {
+// return str.split('').reverse().join('');
+
+
+/////////////////////////////////////////////////////////////
+
+// let revString = '';
+
+// for(let i = str.length - 1; i >= 0; i--){
+//   revString += str[i];
+// }
+
+// return revString;
+
+///////////////////////////////////////////////////////////
+
+
+
+
+
+};
+
+
+
+reverseString('hello');
