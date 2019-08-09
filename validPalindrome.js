a868a20cb50674821166a0946b81c9923faea2db
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-09 11:25:21 +0200

    added validPalindrome.js with comments and solution

---
+++ validPalindrome.js
@@ -0,0 +1,38 @@
+// Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+// Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+// Example 1:
+
+// Input: "A man, a plan, a canal: Panama"
+// Output: true
+// Example 2:
+
+// Input: "race a car"
+// Output: false
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+
+  if(!s) return true;
+  
+  let compareNormal = [];
+  let compareReversed = [];
+  
+      for(let i = 0; i < s.length; i++) {
+      if(s[i] !== " " && s[i].match(/[a-zA-Z0-9]/)) compareNormal.push(s[i].toLowerCase()); 
+      }
+  
+       for(let j = s.length - 1; j >= 0; j--) {
+      if(s[j] !== " " && s[j].match(/[a-zA-Z0-9]/)) compareReversed.push(s[j].toLowerCase()); 
+      }
+  
+      const joinNorm = compareNormal.join('');
+      const joinRev = compareReversed.join('');
+  
+      if(joinNorm === joinRev) return true;
+      else return false;
+  };
