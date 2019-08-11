d71eb05b91e369f8ff782011237e1e691c250bb7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-11 13:40:06 +0200

    added comments to fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,6 +1,6 @@
 const fizzBuzz = () => {
   for(let i = 1; i <= 100; i++){
-    if(i % 15 === 0) console.log('fizzbuzz');
+    if(i % 15 === 0) console.log('fizzbuzz'); // or if(i % 3 === 0 && i % 5 === 0)
     else if(i % 3 === 0) console.log('fizz');
     else if(i % 5 === 0) console.log('buzz');
     else console.log(i);
