b6f2fe475771d451c25232a81158e6b9fea3b400
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-21 12:15:00 +0200

    Added joinElements.js

---
+++ joinElements.js
@@ -0,0 +1,12 @@
+// easy for loop :
+const joinElements = (array, joinString) => {
+  let resultSoFar = '';
+
+  for(let i = 0; i < array.length - 1; i++) {
+    resultSoFar += array[i] + joinString;
+  }
+
+  return resultSoFar + array[array.length - 1];
+}
+
+joinElements(['s', 'cr', 't cod', ' :) :)'], 'e')
