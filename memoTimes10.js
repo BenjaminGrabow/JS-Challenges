14a0bbcf0ffb4231f11c699e3b30395ebc70f808
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-22 23:16:42 +0200

    added memoTimes10.js

--- memoTimes10.js
+++ memoTimes10.js
@@ -1,11 +1,59 @@
+const times10 = (n) => {
+return n * 10;
+};
+
 let cache = {};
 
 const memoTimes10 = (n) => {
-if(cache[n]){
-return cache[n];
-} else {
-  const result = n * 10;
-  cache[n] = result;  
-  return result;
+  if(n in cache) {
+  return cache[n];
+  } else {
+    cache[n] = times10(n);
+    return times10(n);
+  }
 }
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
